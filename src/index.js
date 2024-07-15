6da895b12d92222cca148466da234c2deb3f500d
harisangar2004 <apharisangar4@gmail.com>
2024-07-15 13:27:12 +0530

    change in index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import '../src/index.css';
-import App from './App.jsx'
-import reportWebVitals from './reportWebVitals';
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App/> 
-  </React.StrictMode>
+  <App />
 );
-reportWebVitals();
