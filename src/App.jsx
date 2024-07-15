884d3bd4e82a2c0c04b4b9acca13513cb2ae2068
harisangar2004 <apharisangar4@gmail.com>
2024-07-15 12:33:58 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Todo from './components/Todo';
+
+function App() {
+  
+  return (
+    <div>
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
